9380ef403f0e8d484583e9887098cfa472c2ee56
repl.it user <replituser@example.com>
2021-03-02 10:01:42 +0000

    BinarysearchTree

--- Data Structures/Trees/BinarySearchTree.js
+++ Data Structures/Trees/BinarySearchTree.js
@@ -1,3 +1,71 @@
+class Node {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+
+	searchNode(value) {
+		if(!this.node) {
+			return null;
+		}
+
+		let currentNode = this.root;
+
+		while(currentNode) {
+			if (currentNode.value === value) {
+				return currentNode;
+			} else if (currentNode.value < value) {
+				currentNode = currentNode.right;
+			} else if (currentNode.value > value) {
+				currentNode = currentNode.left;
+			}
+		}
+
+		return null;
+	}
+
+	addNode(value) {
+		const node = new Node(value);
+		let currentNode = this.root;
+		while(currentNode) {
+			if (currentNode.value > value) {
+				if (!currentNode.left) {
+					currentNode.left = node;
+					break;
+				} else {
+					currentNode = currentNode.left;
+				}
+			} else if (currentNode.value < value) {
+				if (!currentNode.right) {
+					currentNode.right = node;
+					break;
+				} else {
+					currentNode = currentNode.right;
+				}
+			} else {
+				return this;
+			}
+		}
+		
+		if (!this.root) {
+			this.root = node;
+		}
+
+		return this;
+	}
+}
+
+
+
+
 class FindBinarySearchTree {
   
   constructor(tree) {
